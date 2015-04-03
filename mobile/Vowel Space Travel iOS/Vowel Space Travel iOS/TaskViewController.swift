c69e6f56a026adfc73f16276523e7418d1933374
Wessel Stoop <woseseltops@hotmail.com>
2015-04-03 16:26:29 +0200

    Set up label location

--- mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/TaskViewController.swift
+++ mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/TaskViewController.swift
@@ -14,17 +14,39 @@ class TaskViewController: UIViewController {
     var tempSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cello", ofType: "wav")!)
     var audioPlayer = AVAudioPlayer()
     
+    var screenWidth: CGFloat?
+    var screenHeight: CGFloat?
+    
     override func viewDidLoad()
     {
+        super.viewDidLoad()
+
+        //Remember the screen sizes
+        self.screenWidth = self.view.frame.size.width;
+        self.screenHeight = self.view.frame.size.height;
+        
+        //Make the background white
+        self.view.backgroundColor = UIColor.whiteColor()
+
+        //Play audio
         audioPlayer = AVAudioPlayer(contentsOfURL: tempSound, error: nil)
         audioPlayer.prepareToPlay()
         audioPlayer.play()
         
-        super.viewDidLoad()
-        println("loaded taskviewer!")
+        //Display a label
+        var label = UILabel();
+        var labelWidth : CGFloat = 300;
+        var labelHeigth : CGFloat = 30;
+        var distanceAboveCenter : CGFloat = 0;
+        
+        label.frame = CGRectMake(0.5*(self.screenWidth!-labelWidth),0.5*(self.screenHeight!-labelHeigth) - distanceAboveCenter,labelWidth,labelHeigth)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "Tap here when you hear a difference"
+        
+        self.view.addSubview(label)
         
     }
-    
+   
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
