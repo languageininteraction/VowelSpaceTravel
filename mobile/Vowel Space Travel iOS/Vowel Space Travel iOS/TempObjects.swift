84a19e4cbf511ec901fd6c095b87d1d770c845c3
Wessel Stoop <woseseltops@hotmail.com>
2015-04-10 12:48:30 +0200

    Added a simple button style for this relatively styleless version

---
+++ mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/TempObjects.swift
@@ -0,0 +1,26 @@
+//
+//  TempObjects.swift
+//  Vowel Space Travel iOS
+//
+//  Created by Wessel Stoop on 10/04/15.
+//  Copyright (c) 2015 Radboud University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TempStyledButton : UIButton
+{
+    required init(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+    required override init(frame CGframe: CGRect)
+    {
+        super.init(frame: CGframe)
+        self.backgroundColor = UIColor.grayColor()
+        self.layer.cornerRadius = 10.0;
+    }
+        
+}
