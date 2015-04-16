396056bf9dd75f5b545df7dfce959f9eb6390a2a
Wessel Stoop <woseseltops@hotmail.com>
2015-04-16 12:46:29 +0200

    Added text input to the login screen

--- mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/LoginViewController.swift
+++ mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/LoginViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    var screenWidth : CGFloat = 0
-    var screenHeight : CGFloat = 0
+    var screenWidth : CGFloat?
+    var screenHeight : CGFloat?
     
     override func viewDidLoad()
     {
@@ -25,9 +25,29 @@ class LoginViewController: UIViewController {
         //Make the background white
         self.view.backgroundColor = UIColor.whiteColor()
         
+        //Create the text fields
+        let inputFieldWidth : CGFloat = 400
+        let inputFieldHeight : CGFloat = 80
+
+        let loginField = UITextField(frame: CGRectMake(0.5*(self.screenWidth!-inputFieldWidth),0.5*(self.screenHeight!-inputFieldHeight)-100,inputFieldWidth,inputFieldHeight))
+        loginField.borderStyle = UITextBorderStyle.RoundedRect
+        loginField.placeholder = "Login name"
+        loginField.font = UIFont(name: "Helvetica", size: 35)
+        self.view.addSubview(loginField)
+
+        let passwordField = UITextField(frame: CGRectMake(0.5*(self.screenWidth!-inputFieldWidth),0.5*(self.screenHeight!-inputFieldHeight),inputFieldWidth,inputFieldHeight))
+        passwordField.borderStyle = UITextBorderStyle.RoundedRect
+        passwordField.placeholder = "Password"
+        passwordField.font = UIFont(name: "Helvetica", size: 35)
+        passwordField.secureTextEntry = true
+        self.view.addSubview(passwordField)
         
         //Create login button
-        let loginButton = TempStyledButton(frame: CGRectMake(100,100,100,100))
+        let buttonWidth : CGFloat = 200
+        let buttonHeight : CGFloat = 70
+        
+        let loginButton = TempStyledButton(frame: CGRectMake(0.5*(self.screenWidth!-buttonWidth),0.5*(self.screenHeight!-buttonHeight)+100,buttonWidth,buttonHeight))
+
         loginButton.setTitle("Login", forState: UIControlState.Normal)
         loginButton.addTarget(self, action: "loginButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(loginButton)
