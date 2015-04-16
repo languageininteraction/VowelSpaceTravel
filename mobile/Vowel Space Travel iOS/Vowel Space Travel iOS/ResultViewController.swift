147daba1d4127bcb04032ef06de2acf8d3291d7a
Wessel Stoop <woseseltops@hotmail.com>
2015-04-16 12:44:48 +0200

    Added a working finish button to the results view

--- mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/ResultViewController.swift
+++ mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/ResultViewController.swift
@@ -20,23 +20,32 @@ class ResultViewController: SubViewController {
         super.viewDidLoad()
         
         //Remember the screen sizes
-        self.screenWidth = self.view.frame.size.width;
-        self.screenHeight = self.view.frame.size.height;
+        self.screenWidth = self.view.frame.size.width
+        self.screenHeight = self.view.frame.size.height
         
         //Make the background white
         self.view.backgroundColor = UIColor.whiteColor()
         
         //Display a label
         var label = UILabel();
-        var labelWidth : CGFloat = 300;
-        var labelHeigth : CGFloat = 30;
-        var distanceAboveCenter : CGFloat = 0;
+        var labelWidth : CGFloat = 300
+        var labelHeigth : CGFloat = 30
+        var distanceAboveCenter : CGFloat = 0
         
         label.frame = CGRectMake(0.5*(self.screenWidth!-labelWidth),0.5*(self.screenHeight!-labelHeigth) - distanceAboveCenter,labelWidth,labelHeigth)
         label.textAlignment = NSTextAlignment.Center
         label.text = "Your score:"
         
         self.view.addSubview(label)
+        
+        //Create the finish button
+        let buttonWidth : CGFloat = 200
+        let buttonHeight : CGFloat = 70
+        
+        let finishButton = TempStyledButton(frame: CGRectMake(0.5*(self.screenWidth!-buttonWidth),0.5*(self.screenHeight!-buttonHeight)+100,buttonWidth,buttonHeight))
+        finishButton.setTitle("Finish", forState: UIControlState.Normal)
+        finishButton.addTarget(self, action: "finishButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(finishButton)
         
     }
     
@@ -46,7 +55,7 @@ class ResultViewController: SubViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func backButtonPressed()
+    func finishButtonPressed()
     {
         self.superController!.subControllerFinished(self)
         self.dismissViewControllerAnimated(false, completion: nil)
