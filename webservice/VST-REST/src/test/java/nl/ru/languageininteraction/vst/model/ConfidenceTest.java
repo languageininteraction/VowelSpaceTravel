afcab2d4108cd40c6b05ae9add2a391368ca7c69
Peter Withers <peter-gthb@bambooradical.com>
2015-05-20 11:57:55 +0200

    Added a test of confidence that includes the CRUD repositories.

--- webservice/VST-REST/src/test/java/nl/ru/languageininteraction/vst/model/ConfidenceTest.java
+++ webservice/VST-REST/src/test/java/nl/ru/languageininteraction/vst/model/ConfidenceTest.java
@@ -17,26 +17,95 @@
  */
 package nl.ru.languageininteraction.vst.model;
 
+import nl.ru.languageininteraction.vst.Application;
+import nl.ru.languageininteraction.vst.rest.ConsonantRepository;
+import nl.ru.languageininteraction.vst.rest.PlayerRepository;
+import nl.ru.languageininteraction.vst.rest.SettingsRepository;
+import nl.ru.languageininteraction.vst.rest.SpeakerRepository;
+import nl.ru.languageininteraction.vst.rest.StimulusResponseRepository;
+import nl.ru.languageininteraction.vst.rest.VowelRepository;
+import nl.ru.languageininteraction.vst.rest.WordRepository;
+import nl.ru.languageininteraction.vst.util.AudioSamplesIngester;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @since May 18, 2015 5:54:30 PM (creation date)
  * @author Peter Withers <p.withers@psych.ru.nl>
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {Application.class})
 public class ConfidenceTest {
+
+    @Autowired
+    private VowelRepository vowelRepository;
+    @Autowired
+    private PlayerRepository playerRepository;
+    @Autowired
+    private StimulusResponseRepository stimulusResultRepository;
+    @Autowired
+    private WordRepository wordsRepository;
+    @Autowired
+    private SpeakerRepository speakerRepository;
+    @Autowired
+    private ConsonantRepository consonantRepository;
+    @Autowired
+    private SettingsRepository settingsRepository;
+    @Autowired
+    private AudioSamplesIngester audioSamplesIngester;
+
+    final Vowel targetVowel = new Vowel();
+    final Vowel standardVowel = new Vowel();
+    final Player player = new Player("a", "b", "c", 1, null);
 
     public ConfidenceTest() {
     }
 
+    private void insertTestData(final int truePositiveCount, final int falsePositiveCount, final int trueNegativeCount, final int falseNegativeCount) {
+        playerRepository.save(player);
+        vowelRepository.save(targetVowel);
+        vowelRepository.save(standardVowel);
+        stimulusResultRepository.deleteAll();
+        for (int truePositiveCounter = 0; truePositiveCounter < truePositiveCount; truePositiveCounter++) {
+            stimulusResultRepository.save(new StimulusResponse(player, targetVowel, standardVowel, true, true, 1));
+        }
+        for (int falsePositiveCounter = 0; falsePositiveCounter < falsePositiveCount; falsePositiveCounter++) {
+            stimulusResultRepository.save(new StimulusResponse(player, targetVowel, standardVowel, false, true, 1));
+        }
+        for (int trueNegativeCounter = 0; trueNegativeCounter < trueNegativeCount; trueNegativeCounter++) {
+            stimulusResultRepository.save(new StimulusResponse(player, targetVowel, standardVowel, false, false, 1));
+        }
+        for (int falseNegativeCounter = 0; falseNegativeCounter < falseNegativeCount; falseNegativeCounter++) {
+            stimulusResultRepository.save(new StimulusResponse(player, targetVowel, standardVowel, true, false, 1));
+        }
+    }
+
     /**
-     * Test of getting confidence via the CRUD DB with inserted user results.
+     * Test of getLowerBound via the CRUD DB with inserted user results.
      */
     @org.junit.Test
-    public void testViaCrudDb() {
-        System.out.println("testViaCrudDb");
-        // todo: add a unit test that inserts into the database and gets the confidence from the query results
-        fail("The test case is a prototype.");
+    public void testGetLowerBoundViaCrudDb() {
+        System.out.println("testGetLowerBoundViaCrudDb");
+        insertTestData(8, 8, 8, 8);
+        assertEquals(0.3315, new Confidence(stimulusResultRepository, targetVowel, standardVowel).getLowerBound(), 0.01);
+        insertTestData(0, 8, 8, 8);
+        assertEquals(0.1797, new Confidence(stimulusResultRepository, targetVowel, standardVowel).getLowerBound(), 0.01);
+    }
+
+    /**
+     * Test of getUpperBound via the CRUD DB with inserted user results.
+     */
+    @org.junit.Test
+    public void testGetUpperBoundViaCrudDb() {
+        System.out.println("testGetUpperBoundViaCrudDb");
+        insertTestData(8, 8, 8, 8);
+        assertEquals(0.66, new Confidence(stimulusResultRepository, targetVowel, standardVowel).getUpperBound(), 0.01);
+        insertTestData(0, 8, 8, 8);
+        assertEquals(0.5329, new Confidence(stimulusResultRepository, targetVowel, standardVowel).getUpperBound(), 0.01);
     }
 
     /**
