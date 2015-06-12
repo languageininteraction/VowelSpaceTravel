96b237d781e16c3f06b274af740d26313a196f14
Wessel Stoop <woseseltops@hotmail.com>
2015-06-12 17:24:17 +0200

    Added round constants

--- mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/Constants.swift
+++ mobile/Vowel Space Travel iOS/Vowel Space Travel iOS/Constants.swift
@@ -14,3 +14,5 @@ let kSpeedUpDownload = kDevelopmentMode ? true : false
 let kWebserviceURL = kDevelopmentMode ? "http://applejack.science.ru.nl:8080/" : ""
 
 let kTimeBetweenStimuli : Double = 2
+let kPauseBetweenRounds : Double = 6
+let kNumberOfStimuliInRound : Int = 10 //Not used yet
